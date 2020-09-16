98656cdf6e3c337f9fca82784d1a32b887d8f073
Zefiruu <53922624+Zefiruu@users.noreply.github.com>
2020-09-16 23:48:22 +0100

    add clock project

---
+++ clock/script.js
@@ -0,0 +1,19 @@
+function clock(){
+    setTimeout(
+        () => {
+        let now = new Date(),
+            sec = (now.getSeconds() * 6) - 90,
+            min = (now.getMinutes() * 6) - 90,
+            hour = (now.getHours() * 30) - 90;
+            document.getElementById('second-hand').style.transform = 'rotate('+sec+'deg)';
+            document.getElementById('minute-hand').style.transform = 'rotate('+min+'deg)';
+            document.getElementById('hour-hand').style.transform = 'rotate('+hour+'deg)';
+
+            console.log(min)
+            if(sec < 1000) {
+                clock()
+            }
+        }, 1000
+    )
+}
+clock();
