22ab67c9030df1e60cf309a19501187235d2858b
Zefiruu <53922624+Zefiruu@users.noreply.github.com>
2020-09-16 02:16:44 +0100

    Initial commit

---
+++ drumKit/dom.js
@@ -0,0 +1,29 @@
+function playSound(sound, currentTimez = 0) {
+    let audio = new Audio('media/'+sound+'.mp3');
+    audio.currentTime = currentTimez;
+    audio.play();
+} 
+let scale = key => {
+        let element = document.getElementById(key);
+        element.classList.add('scale');
+        setTimeout(()=> element.classList.remove('scale'), 100);
+    };
+
+document.addEventListener('keydown', e => {
+    switch(e.key) {
+        case 'd':
+            playSound('kiero');
+            break;
+        case 'f':
+            playSound('nya');
+            break;
+        case 'j':
+            playSound('ooh', 1);
+            break;
+        case 'k':
+            playSound('yoshi');
+            break;
+        }
+    scale(e.key)
+});
+
