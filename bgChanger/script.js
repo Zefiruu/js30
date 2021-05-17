f57f7742e17b8029d29006a902a997cedc70dba3
Zefiru <zefiru@protonmail.com>
2021-05-17 21:18:43 +0100

    Improve layout and styling

--- bgChanger/script.js
+++ bgChanger/script.js
@@ -1,6 +1,10 @@
-function change() {
+function changeBackground() {
     let newColor = document.getElementById('color-picker').value;
-    document.documentElement.style.cssText = `--var1: ${newColor};`;
+    document.documentElement.style.cssText = `--background-color: ${newColor};`;
 }
+
+function
+
+
 let arr = document.querySelectorAll('input');
-arr.forEach(input => input.addEventListener('mousemove', () => console.log('')))
+arr.forEach(input => input.addEventListener('pointermove', (event) => console.log(event.target.value)))
