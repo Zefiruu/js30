729471f1aae30a4da004336bed968917da41df76
Zefiru <zefiru@protonmail.com>
2021-05-17 23:41:47 +0100

    Formatted code for better readability

--- clock/script.js
+++ clock/script.js
@@ -1,19 +1,20 @@
-function clock(){
-    setTimeout(
-        () => {
-        let now = new Date(),
-            sec = (now.getSeconds() * 6) - 90,
-            min = (now.getMinutes() * 6) - 90,
-            hour = (now.getHours() * 30) - 90;
-            document.getElementById('second-hand').style.transform = 'rotate('+sec+'deg)';
-            document.getElementById('minute-hand').style.transform = 'rotate('+min+'deg)';
-            document.getElementById('hour-hand').style.transform = 'rotate('+hour+'deg)';
+function clock() {
+	setTimeout(() => {
+		let now = new Date(),
+			sec = now.getSeconds() * 6 - 90,
+			min = now.getMinutes() * 6 - 90,
+			hour = now.getHours() * 30 - 90;
+		document.getElementById("second-hand").style.transform =
+			"rotate(" + sec + "deg)";
+		document.getElementById("minute-hand").style.transform =
+			"rotate(" + min + "deg)";
+		document.getElementById("hour-hand").style.transform =
+			"rotate(" + hour + "deg)";
 
-            console.log(min)
-            if(sec < 1000) {
-                clock()
-            }
-        }, 1000
-    )
+		console.log(min);
+		if (sec < 1000) {
+			clock();
+		}
+	}, 1000);
 }
 clock();
--- drumKit/dom.js
+++ drumKit/dom.js
@@ -1,29 +1,28 @@
 function playSound(sound, currentTimez = 0) {
-    let audio = new Audio('media/'+sound+'.mp3');
-    audio.currentTime = currentTimez;
-    audio.play();
-} 
-let scale = key => {
-        let element = document.getElementById(key);
-        element.classList.add('scale');
-        setTimeout(()=> element.classList.remove('scale'), 100);
-    };
+	let audio = new Audio("media/" + sound + ".mp3");
+	audio.currentTime = currentTimez;
+	audio.play();
+}
+let scale = (key) => {
+	let element = document.getElementById(key);
+	element.classList.add("scale");
+	setTimeout(() => element.classList.remove("scale"), 100);
+};
 
-document.addEventListener('keydown', e => {
-    switch(e.key) {
-        case 'd':
-            playSound('kiero');
-            break;
-        case 'f':
-            playSound('nya');
-            break;
-        case 'j':
-            playSound('ooh', 1);
-            break;
-        case 'k':
-            playSound('yoshi');
-            break;
-        }
-    scale(e.key)
+document.addEventListener("keydown", (e) => {
+	switch (e.key) {
+		case "d":
+			playSound("kiero");
+			break;
+		case "f":
+			playSound("nya");
+			break;
+		case "j":
+			playSound("ooh", 1);
+			break;
+		case "k":
+			playSound("yoshi");
+			break;
+	}
+	scale(e.key);
 });
-
